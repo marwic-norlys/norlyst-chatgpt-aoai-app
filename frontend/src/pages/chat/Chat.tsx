29df56185cb4f2737fe7241241912afc0a1be2ab
Martin Wickings <marwic@norlys.dk>
2023-10-20 12:31:32 +0000

    Styling til HR POC

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -555,7 +555,7 @@ const Chat = () => {
                                     aria-hidden="true"
                                 />
                                 <h1 className={styles.chatEmptyStateTitle}>Start din chat</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>(Du snakker med gpt-35-turbo-16k, feedback til marwic@norlys.dk)</h2>
+                                <h2 className={styles.chatEmptyStateSubtitle}>(Du snakker med gpt-4-32k, feedback til marwic@norlys.dk)</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -79,7 +79,7 @@ const Layout = () => {
                             aria-hidden="true"
                         />
                         <Link to="#" className={styles.headerTitleContainer}>
-                            <h1 className={styles.headerTitle}>ChatGPT</h1>
+                            <h1 className={styles.headerTitle}>HR ChatGPT</h1>
                         </Link>
                     </Stack>
                     <Stack horizontal tokens={{ childrenGap: 4 }}>
